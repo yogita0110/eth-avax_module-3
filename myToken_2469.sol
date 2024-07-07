e9b099322220af22d651eea758096328a2203523
yogita0110 <149076335+yogita0110@users.noreply.github.com>
2024-07-07 17:10:12 +0530

    Update and rename myToken.sol to myToken_2469.sol

---
+++ myToken_2469.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken_21BCS2469 is ERC20 {
+    address public owner;
+
+    constructor() ERC20("MyToken", "MTK") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+}
